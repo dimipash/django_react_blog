0beecf73f5427757c96cb5c98b1d119b15d5e4b3
Dimitar Pashev <dim.pashev@gmail.com>
2024-07-23 14:23:24 +0300

    dashboard post list API view

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -15,4 +15,8 @@ urlpatterns = [
     path("post/comment-post/", api_views.PostCommentAPIView.as_view()),
     path("post/bookmark-post/", api_views.BookmarkPostAPIView.as_view()),
     path("author/dashboard/stats/<user_id>/", api_views.DashboardStats.as_view()),
+    path("author/dashboard/comment-list/<user_id>/", api_views.DashboardCommentLists.as_view()),
+    path("author/dashboard/noti-list/<user_id>/", api_views.DashboardNotificationList.as_view()),
+    path("author/dashboard/noti-mark-seen/", api_views.DashboardMarkNotificationAsSeen.as_view()),
+    path("author/dashboard/reply-comment/", api_views.DashboardReplyCommentAPIView.as_view()),
 ]
